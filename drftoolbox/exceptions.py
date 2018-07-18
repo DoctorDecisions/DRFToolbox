a9066c5759c2df1354536f1b092a2e9a770a40d7
Eric Buckley <eric.buckley@gmail.com>
2018-07-17 17:11:43 -0700

    adding drftoolbox exceptions

---
+++ drftoolbox/exceptions.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+    drftoolbox.exceptions
+    ~~~~~~~~~~~~~~~~~~~~~
+
+    This module defines exception classes used by the API
+
+    :copyright: (c) 2018 by Medical Decisions LLC
+"""
+
+
+class JWTMismatchClaimException(Exception):
+    pass
